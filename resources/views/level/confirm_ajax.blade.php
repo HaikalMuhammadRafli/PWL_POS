2a61adb833663fd08d081f155ce11add044abd13
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2025-03-18 05:13:17 +0700

    refactor: J6 T P3 fixed missing minus

--- resources/views/level/confirm_ajax.blade.php
+++ resources/views/level/confirm_ajax.blade.php
@@ -23,7 +23,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Hapus Data Level</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria label="Close"><span
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
                             aria-hidden="true">&times;</span></button>
                 </div>
                 <div class="modal-body">
