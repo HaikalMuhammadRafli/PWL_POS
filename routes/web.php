86bc3b7f58b79b82051eefdec6222883944f678d
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2025-04-15 02:23:32 +0700

    feat: J8 T3 add PDF export functionality for user, level, kategori, and supplier data

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,7 @@ Route::middleware(['auth'])->group(function () {
             Route::get('/import', [UserController::class, 'import']);
             Route::post('/import_ajax', [UserController::class, 'import_ajax']);
             Route::get('/export_excel', [UserController::class, 'export_excel']);
+            Route::get('/export_pdf', [UserController::class, 'export_pdf']);
             Route::get('/{id}', [UserController::class, 'show']);
             Route::get('/{id}/show_ajax', [UserController::class, 'show_ajax']);
             Route::get('/{id}/edit', [UserController::class, 'edit']);
@@ -63,6 +64,7 @@ Route::middleware(['auth'])->group(function () {
             Route::get('/import', [LevelController::class, 'import']);
             Route::post('/import_ajax', [LevelController::class, 'import_ajax']);
             Route::get('/export_excel', [LevelController::class, 'export_excel']);
+            Route::get('/export_pdf', [LevelController::class, 'export_pdf']);
             Route::get('/{id}', [LevelController::class, 'show']);
             Route::get('/{id}/show_ajax', [LevelController::class, 'show_ajax']);
             Route::get('/{id}/edit', [LevelController::class, 'edit']);
@@ -86,6 +88,7 @@ Route::middleware(['auth'])->group(function () {
             Route::get('/import', [KategoriController::class, 'import']);
             Route::post('/import_ajax', [KategoriController::class, 'import_ajax']);
             Route::get('/export_excel', [KategoriController::class, 'export_excel']);
+            Route::get('/export_pdf', [KategoriController::class, 'export_pdf']);
             Route::get('/{id}', [KategoriController::class, 'show']);
             Route::get('/{id}/show_ajax', [KategoriController::class, 'show_ajax']);
             Route::get('/{id}/edit', [KategoriController::class, 'edit']);
@@ -108,6 +111,7 @@ Route::middleware(['auth'])->group(function () {
             Route::get('/import', [SupplierController::class, 'import']);
             Route::post('/import_ajax', [SupplierController::class, 'import_ajax']);
             Route::get('/export_excel', [SupplierController::class, 'export_excel']);
+            Route::get('/export_pdf', [SupplierController::class, 'export_pdf']);
             Route::get('/{id}', [SupplierController::class, 'show']);
             Route::get('/{id}/show_ajax', [SupplierController::class, 'show_ajax']);
             Route::get('/{id}/edit', [SupplierController::class, 'edit']);
