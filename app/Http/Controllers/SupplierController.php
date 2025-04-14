7a1b5200c26c309c756c88b25960c0bf638344ef
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2025-04-15 02:23:10 +0700

    feat: J8 T3 add PDF export functionality for supplier data

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\SupplierModel;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use PhpOffice\PhpSpreadsheet\IOFactory;
@@ -389,5 +390,17 @@ class SupplierController extends Controller
 
         $writer->save('php://output');
         exit;
+    }
+
+    public function export_pdf()
+    {
+        $supplier = SupplierModel::select('supplier_kode', 'supplier_nama', 'supplier_alamat')->get();
+
+        $pdf = Pdf::loadView('supplier.export_pdf', ['supplier' => $supplier]);
+        $pdf->setPaper('a4', 'portrait');
+        $pdf->setOption("isRemoteEnabled", true);
+        $pdf->render();
+
+        return $pdf->stream('Data Supplier ' . date('Y-m-d H:i:s') . '.pdf');
     }
 }
--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -8,7 +8,10 @@
                 <button onclick="modalAction('{{ url('supplier/import') }}')" class="btn btn-info">Import Supplier</button>
                 <a href="{{ url('supplier/export_excel') }}" class="btn btn-primary"><i class="fa fa-file-excel"></i> Export
                     Excel</a>
-                <button onclick="modalAction('{{ url('supplier/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah
+                <a href="{{ url('supplier/export_pdf') }}" class="btn btn-warning"><i class="fa fa-file-pdf"></i> Export
+                    Pdf</a>
+                <button onclick="modalAction('{{ url('supplier/create_ajax') }}')"
+                    class="btn btn-sm btn-success mt-1">Tambah
                     Ajax</button>
             </div>
         </div>
